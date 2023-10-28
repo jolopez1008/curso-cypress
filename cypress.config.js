8a4f69fbd7c35a5a8caa50acbdde55c94d393b7e
Josue Lopez <josue.freelancer@gmail.com>
2023-10-28 04:22:29 -0400

    Project updated

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,9 @@
-const { defineConfig } = require("cypress");
-
-module.exports = defineConfig({
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+module.exports = {
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      allureWriter(on, config);
+            return config;
     },
   },
-});
+};
